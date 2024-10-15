9d6cd72db5c6c6b991d2d6d17e6d25d0dafef529
DipeshMajithiaPCSI <dipesh@pcsionweb.com>
2024-10-15 12:29:42 +0530

    login signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import PublicBase from "./pages/Public/PublicBase";
-
+import Home from "./pages/Protected/Home";
+import Login from "../src/components/Login";
+import { useState } from "react";
+import SignUp from "./components/SignUp";
 function App() {
+  const [pageType, setPageType] = useState("login");
+  console.log("PageType", pageType);
   return (
     <div className="App">
-      <PublicBase>
-        <h1>I am a child</h1>
-      </PublicBase>
+      {/* <PublicBase>
+        {pageType === "login" ? (
+          <Login setPageType={setPageType} />
+        ) : (
+          <SignUp setPageType={setPageType} />
+        )}
+      </PublicBase> */}
+      <Home />
     </div>
   );
 }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import styles from "../assets/css/Login.module.css";
-function Login({ setPageType }) {
+import styles from "../assets/css/SignUp.module.css";
+function SignUp({ setPageType }) {
   return (
     <div className={styles.sectionContainer}>
       <p className={styles.titleText}>Sign In</p>
       <p>Welcome back! Enter your email and password below to sign in.</p>
       <div className={styles.inputSection}>
+        <div className={styles.inputItem}>
+          <label>Full Name</label>
+          <input placeholder="Full Name" className={styles.inputbox} />
+        </div>
         <div className={styles.inputItem}>
           <label>Email</label>
           <input placeholder="Email" className={styles.inputbox} />
@@ -14,29 +18,22 @@ function Login({ setPageType }) {
           <label>Password</label>
           <input placeholder="Password" className={styles.inputbox} />
         </div>
-      </div>
-      <div className={styles.rmbme}>
-        <div className={styles.rmbmeleft}>
-          <input type="checkbox" />
-          <label>Remember me</label>
-        </div>
-        <div className={styles.rmbmeleft}>
-          <p className={styles.fgtpwdtext} onClick={() => {}}>
-            Forgot Password?
-          </p>
+        <div className={styles.inputItem}>
+          <label>Confirm Password</label>
+          <input placeholder="Confirm Password" className={styles.inputbox} />
         </div>
       </div>
       <div className={styles.buttonContainer}>
         <button className={styles.btnSignIn}>Sign In</button>
         <p>
-          Not a member yet?{" "}
+          Already a member?{" "}
           <span
             onClick={() => {
-              setPageType("signup");
+              setPageType("login");
             }}
             style={{ color: "#1488CC" }}
           >
-            Sign Up
+            Sign In
           </span>
         </p>
       </div>
@@ -44,4 +41,4 @@ function Login({ setPageType }) {
   );
 }
 
-export default Login;
+export default SignUp;
